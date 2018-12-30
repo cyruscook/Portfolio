fecc89a7e20fb129a4a02f406519201f7c6d97e4
Cyrus Cook <11858105+cyruscook@users.noreply.github.com>
2018-12-30 19:43:29 +0000

    Revamped Loading screen
    Old one wasnt working

--- script.js
+++ script.js
@@ -1,33 +1,4 @@
-// Thank you https://blog.hellojs.org/create-a-very-basic-loading-screen-using-only-javascript-css-3cf099c48b19
 var pageloaded = false;
-
-$(document).ready(function() {
-	var loadingtimeout = setTimeout(function(){
-		var div = $('#loading');
-		if(!pageloaded){
-			switch(div.text()){
-				case "Loading":
-					div.text("Loading.");
-					break;
-				case "Loading.":
-					div.text("Loading..");
-					break;
-				case "Loading..":
-					div.text("Loading...");
-					break;
-				case "Loading...":
-					div.text("Loading");
-					break;
-				default:
-					div.text("Loading");
-					break;
-			}
-		} else{
-			div.remove();
-			clearTimeout(loadingtimeout);
-		}
-	},10);
-});
 
 // Thanks https://stackoverflow.com/a/34579496/7641587
 function readTextFile(file, callback) {
