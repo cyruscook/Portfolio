c2239c8ba61ced27e8c82e9354757f725f0f150f
Cyrus Cook <11858105+cyruscook@users.noreply.github.com>
2019-01-02 18:38:57 +0000

    Fixed syntax error

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ function writeVars(){
 			var margintop = "";
 			if(get_if_exist(card.img) != undefined){
 				imagecode = `<center><img src="${card.img.url}" style="margin-top: calc(( ${card.img.height} / 2) * -1); background-color: #666; width: ${card.img.width}; height: ${card.img.width}; border-radius: ${card.img["border-radius"]};"></img></center>`;
-				margintop = card.img.height * 0.75;
+				margintop = `calc(${card.img.height} * 0.75)`;
 			}
 			
 			var thiscard = `
